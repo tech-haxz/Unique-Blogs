09d5bd03ab67ff52a7a74f8af4d9d8d16592100f
tech-haxz <ranktopno1@gmail.com>
2025-11-01 13:36:07 +0530

    Backend code modified!

--- api/database.py
+++ api/database.py
@@ -4,10 +4,12 @@ from sqlmodel import Session, SQLModel, create_engine
 
 
 sqlite_file_name = "./blog.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+postgres_url = "postgresql://database_0iv1_user:aLtBa5eP5bsCbr5M7Y1QyRBIyhEvxc9I@dpg-d42rt6gdl3ps73cnkjv0-a/database_0iv1"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(postgres_url, connect_args=connect_args)
 
 
 def create_db_and_tables():
